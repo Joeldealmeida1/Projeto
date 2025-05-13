1152c3c5c58de02fcfbb514f8bf2ea4de3b0dd27
Joeldealmeida1 <almeida.joel@escola.pr.gov.br>
2025-05-13 19:16:54 -0300

    salvar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 77;
-let chute = prompt('Escolha um número entre 1 e 100');
+let chute = prompt('Escolha um número entre 1 e 1000');
 
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    console.log('Isso ai! Você descobriu o número secreto (77)');
 }
